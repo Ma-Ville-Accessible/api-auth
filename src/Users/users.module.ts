f6d59d3e08cdc8374cf18318bdff35e77a70fdcf
Simon Deflesschouwer <43884154+defless@users.noreply.github.com>
2023-02-16 09:03:45 +0100

    fix: fix imports

--- src/Users/users.module.ts
+++ src/Users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { UsersController } from './Users.controller';
-import { UsersService } from './Users.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { User, UserSchema } from '../core/schemas/Users.schema';
 
 @Module({
