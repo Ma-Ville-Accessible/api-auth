80f0e6190f4c7d6d68c92bc5e8577e3f395a7a1b
Morgan Millet <millet.morgan5@gmail.com>
2023-08-07 18:58:04 +0200

    feat(user): add role to user schema

--- src/core/schemas/users.schema.ts
+++ src/core/schemas/users.schema.ts
@@ -31,6 +31,9 @@ export class User {
 
   @Prop()
   usedRefresh: [string];
+
+  @Prop({ required: true, enum: ['user', 'admin'], default: 'user' })
+  role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
