78bfe9f969de4635b8894a56391e216242859ea4
Simon Deflesschouwer <43884154+defless@users.noreply.github.com>
2023-08-24 14:20:19 +0200

    feat: production ready

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   isAlive(): string {
-    return 'Its alive!';
+    return 'Ma Ville Accessible - Auth_api';
   }
 }
